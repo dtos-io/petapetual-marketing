e0a009903e7e2896b9181d5a210ed4e70dd64518
Dustin Chung <dustin.chung@macbookair.lan>
2025-11-04 13:24:52 +0800

    Add Pricing page basics

---
+++ src/components/pricing_table/index.ts
@@ -0,0 +1,5 @@
+export { default as FreeTier } from "./FreeTier.astro";
+export { default as BasicTier } from "./BasicTier.astro";
+export { default as EssentialsTier } from "./EssentialsTier.astro";
+export { default as PremiumTier } from "./PremiumTier.astro";
+export { default as UnlimitedTier } from "./UnlimitedTier.astro";
